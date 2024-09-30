444e76e5c6a1b05d06aad948af4f9ffe5dbc5f90
MrWalt <mihaelkrstevski.2002@yahoo.com>
2024-09-30 14:40:43 +0200

    Refactor Header dropdown menu into its own ui component

---
+++ src/ui/DropDownMenu.jsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import {
+  computerPeripherals,
+  desktopComputerParts,
+  other,
+} from "../data/productTabs";
+import { NavLink } from "react-router-dom";
+
+const StyledDiv = styled.div`
+  width: 28rem;
+  height: calc(100vh - 6.4rem);
+  background-color: var(--color-grey-200);
+
+  /* border-top: 1px solid var(--color-brand-400); */
+  border-right: 1px solid var(--color-brand-400);
+
+  color: var(--color-grey-900);
+
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  transform: translate(-150%, 100%);
+  transition: all 0.3s ease-out;
+
+  opacity: 0;
+  visibility: hidden;
+  pointer-events: none;
+
+  display: flex;
+  flex-direction: column;
+
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const ProductLink = styled(NavLink)`
+  padding: 0.6rem 2.4rem;
+  transition: var(--animation-default);
+
+  &:hover {
+    background-color: var(--color-brand-200);
+    border-left: 8px solid var(--color-brand-800);
+    padding-left: 3.2rem;
+  }
+
+  &:active {
+    border-left: 64px solid var(--color-brand-800);
+  }
+`;
+
+export default function DropDownMenu() {
+  return (
+    <StyledDiv className="dropdown-menu">
+      <span>&mdash; Desktop Computers</span>
+      {desktopComputerParts.map((item) => (
+        <ProductLink to={`products/${item.link}`}>{item.title}</ProductLink>
+      ))}
+      <span>&mdash; Computer Peripherals</span>
+      {computerPeripherals.map((item) => (
+        <ProductLink to={`products/${item.link}`}>{item.title}</ProductLink>
+      ))}
+      <span>&mdash; Other</span>
+      {other.map((item) => (
+        <ProductLink to={`products/${item.link}`}>{item.title}</ProductLink>
+      ))}
+    </StyledDiv>
+  );
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,5 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import {
-  computerPeripherals,
-  desktopComputerParts,
-  other,
-} from "../data/productTabs";
 
 import {
   HiOutlineBars3,
@@ -12,6 +7,8 @@ import {
   HiOutlineUserCircle,
   HiOutlineShoppingCart,
 } from "react-icons/hi2";
+
+import DropDownMenu from "./DropDownMenu";
 
 const StyledHeader = styled.header`
   position: fixed;
@@ -98,51 +95,6 @@ const LinkContainer = styled.div`
   align-items: center;
 `;
 
-const DropDownMenu = styled.div`
-  width: 28rem;
-  height: calc(100vh - 6.4rem);
-  background-color: var(--color-grey-200);
-  /* border-top: 1px solid var(--color-brand-400); */
-  border-right: 1px solid var(--color-brand-700);
-
-  color: var(--color-grey-900);
-
-  position: absolute;
-  bottom: 0;
-  left: 0;
-
-  transform: translate(-150%, 100%);
-  transition: all 0.3s ease-out;
-
-  opacity: 0;
-  visibility: hidden;
-  pointer-events: none;
-
-  display: flex;
-  flex-direction: column;
-
-  overflow-y: scroll;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const ProductLink = styled(NavLink)`
-  padding: 0.6rem 2.4rem;
-  transition: var(--animation-default);
-
-  &:hover {
-    background-color: var(--color-brand-200);
-    border-left: 8px solid var(--color-brand-800);
-    padding-left: 3.2rem;
-  }
-
-  &:active {
-    border-left: 64px solid var(--color-brand-800);
-  }
-`;
-
 export default function Header() {
   return (
     <StyledHeader>
@@ -151,21 +103,7 @@ export default function Header() {
           <HiOutlineBars3 />
           Products
         </ProductsLink>
-        {/* <StyledLink to="/">LOGO</StyledLink> */}
-        <DropDownMenu className="dropdown-menu">
-          <span>&mdash; Desktop Computers</span>
-          {desktopComputerParts.map((item) => (
-            <ProductLink to={`products/${item.link}`}>{item.title}</ProductLink>
-          ))}
-          <span>&mdash; Computer Peripherals</span>
-          {computerPeripherals.map((item) => (
-            <ProductLink to={`products/${item.link}`}>{item.title}</ProductLink>
-          ))}
-          <span>&mdash; Other</span>
-          {other.map((item) => (
-            <ProductLink to={`products/${item.link}`}>{item.title}</ProductLink>
-          ))}
-        </DropDownMenu>
+        <DropDownMenu />
         <LinkContainer>
           <StyledLink to={`contact`}>
             <HiOutlineChatBubbleLeftRight />
